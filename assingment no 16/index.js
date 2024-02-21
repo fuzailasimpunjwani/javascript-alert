67ec7d2996c85c03b872ec0be337c600abe1b3d5
Fuzail Punjwani <160146425+fuzailasimpunjwani@users.noreply.github.com>
2024-02-22 00:15:04 +0500

    Add files via upload

---
+++ assingment no 16/index.js
@@ -0,0 +1,53 @@
+//   ................chapter 1..................
+
+alert("Error! please enter the right password")
+alert("Welcome to JS land...\nHappy Coding!")
+alert("Welcome to JS land...")
+alert("Happy Coding!\nPrevent this pagefrom creating additional dialouges.")
+alert("Hello... I can run JS through my web browser's console")
+
+// .................chapter 2....................
+
+var firstname = "Jhone" 
+var lastname = " Doe"
+alert(firstname+lastname)
+
+var age = "15"
+var name = " Years old"
+alert(age+name)
+
+var text = "Certified Mobile Application Development"
+alert(text)
+
+var item = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+alert(item)
+
+var text = "My Email Address is example@example.com"
+alert(text)
+
+var text = "I am trying to learn the bok A smarter\nway to learn JavaScript"
+alert(text)
+
+var text = "Yah! <b>I</b> can write <b>HTML</b> content through JavaScript"
+document.write(text)
+
+var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+alert(design)
+
+// ...................Chapter 3...................
+
+var age = (15)
+alert("I am " +age+ " years old")
+
+var number = (14)
+alert("You have visited thi site "+number+ "    Times")
+
+var birth = (1990)
+document.write("My Birth year is "+birth)
+document.write("<br>")
+document.write("Data type of my declared variable is number")
+
+var visitername = ("<b>Jhone Doe</b>")
+var title = ("<b> T-shirts</b>")
+var quantity = ("<b> 5</b>")
+document.write(visitername+" ordered "+quantity + title+" (s) on XYZ Clothing store")
